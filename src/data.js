5666b0775096cd0cd891cef9e18f17af0d2b1e49
Bruno Pontes <bruno_confessor@hotmail.com>
2022-04-07 19:34:43 -0300

    Troca de imagem post

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export function getPost(){
         id: uuid(),
         user: user,
         text: "Olá Mundo!",
-        img: "https://icapps.com/uploads/site/what-is-the-right-background-for-a-react-native-developer/_800x418_crop_center-center_82_none/React_Native_image.jpg?mtime=1594715542",
+        img: "https://img.wattpad.com/a00e69f067025077ad0a8cd70fe531ce3ac681a9/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f634c43346c326936624e344572673d3d2d33372e313537643266613130633131306239373436333835393035363133362e6a7067?s=fit&w=720&h=720",
         like: 1,
         comments: [{
             id: uuid(),
